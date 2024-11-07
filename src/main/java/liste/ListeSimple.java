13fc3729a452a100acd5c02411ad5c86c6664a11
AxLaiR <axel.clareton@alumni.univ-avignon.fr>
2024-11-07 18:12:56 +0100

    test coverage

--- src/main/java/liste/ListeSimple.java
+++ src/main/java/liste/ListeSimple.java
@@ -120,26 +120,24 @@ public class ListeSimple {
     public void echanger(Noeud r1, Noeud r2) {
         if (r1 == r2)
             return;
-        Noeud precedentR1;
-        Noeud precedentR2;
-        if (r1 != tete && r2 != tete) {
-            precedentR1 = getPrecedent(r1);
-            precedentR2 = getPrecedent(r2);
+        Noeud precedentR1 = getPrecedent(r1);
+        Noeud precedentR2 = getPrecedent(r2);
+    
+        if (precedentR1 != null) {
             precedentR1.setSuivant(r2);
-            precedentR2.setSuivant(r1);
-        } else if (r1 == tete) {
-            precedentR2 = getPrecedent(r2);
-            precedentR2.setSuivant(tete);
+        } else {
             tete = r2;
         }
-        else {
-            precedentR1 = getPrecedent(r1);
-            precedentR1.setSuivant(tete);
+    
+        if (precedentR2 != null) {
+            precedentR2.setSuivant(r1);
+        } else {
             tete = r1;
         }
-        Noeud temp = r2.getSuivant();
-        r2.setSuivant(r1.getSuivant());
-        r1.setSuivant(temp);
+    
+        Noeud temp = r1.getSuivant();
+        r1.setSuivant(r2.getSuivant());
+        r2.setSuivant(temp);
     }
 
 }
